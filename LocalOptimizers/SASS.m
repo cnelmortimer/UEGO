3b2c8b1bc8cb76c4f456c47d5dd66dfec2728fa6
Nicolás C <ncalvocruz@gmail.com>
2019-02-17 21:36:25 +0100

    Avance en SASS. Me queda escalar la perturbacion

--- LocalOptimizers/SASS.m
+++ LocalOptimizers/SASS.m
@@ -7,11 +7,64 @@ end
 
 % Internal auxiliary functions:
 function config = buildConfig()
-    config.name = 'SASS';
+    config.name = 'SASS'; % Default configuration to work with
+    config.Scnt = 5; % Minimum number of success
+    config.Fcnt = 3; % Minimum number of failures
+    config.ex = 2.0; % Expansion
+    config.ct = 0.5; % Constraction
+    config.sig_ub = 1.0; % Upper perturbation limit
+    config.sig_lb = 0.00001; % Lower perturbation limit
+    config.MaxFcnt = 32; % Maximum number of consecutive fails to stop (included from V. Plaza's TFM)
 end
 
-function [new_point, new_value] = SASS_optimizer(start_point, radius, initial_val, bounds, func, max_steps, optimizer_config)
-    new_point = [];
-    new_value = 0;
-    disp(optimizer_config.name);
+function [x, current_value] = SASS_optimizer(start_point, radius, initial_val, bounds, func, max_steps, optimizer_config)
+    x = start_point;
+    current_value = initial_val;
+    dim = size(bounds, 1); % Number of dimensions
+    b = zeros(dim, 1); % Bias
+    k = 0; % Iteration counter
+    scnt = 0; % Success counter
+    fcnt = 0; % Failure counter
+    sigma = optimizer_config.sig_ub;
+    while k<max_steps && fcnt < optimizer_config.MaxFcnt
+        if sigma < optimizer_config.sig_lb || sigma > optimizer_config.sig_ub % The sigma > ub is included in Jelasity's...
+            sigma = optimizer_config.sig_ub; % scnt = 0;% Jelasity fcnt = 0;% Jelasity b = 0*b;% Jelasity (Restart)
+        end
+        if scnt > optimizer_config.Scnt
+            sigma = optimizer_config.ex * sigma;
+        end
+        if fcnt > optimizer_config.Fcnt
+            sigma = optimizer_config.ct * sigma;
+        end
+        % Generate a multivariate Gaussian vector (perturbation):
+        xi = b + normrnd(0, sigma, [dim, 1]);
+        x_prime = x + xi;
+        x_prime(x_prime < bounds(:, 1)) = bounds(x_prime < bounds(:, 1), 1); % If too small: saturate
+        x_prime(x_prime > bounds(:, 2)) = bounds(x_prime > bounds(:, 2), 2); % If too big: saturate
+        val_prime = func(x_prime);
+        if val_prime < current_value % Move:
+            x = x_prime;
+            current_value = val_prime;
+            b = 0.2*b + 0.4*xi;
+            scnt = scnt+1; 
+            fcnt = 0;
+        else
+            x_prime = x - xi;
+            x_prime(x_prime < bounds(:, 1)) = bounds(x_prime < bounds(:, 1), 1); % If too small: saturate
+            x_prime(x_prime > bounds(:, 2)) = bounds(x_prime > bounds(:, 2), 2); % If too big: saturate
+            val_prime = func(x_prime);
+            if val_prime < current_value
+                x = x_prime;
+                current_value = val_prime;
+                b = b - 0.4*xi;
+                scnt = scnt+1; 
+                fcnt = 0;
+            else
+                b = 0.5*b;
+                fcnt = fcnt+1; 
+                scnt = 0;
+            end
+        end
+        k = k + 1;
+    end
 end
