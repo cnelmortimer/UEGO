fbf4a2a27ba10dab3bf378cc79e8879ed01c664c
Nicolás C <ncalvocruz@gmail.com>
2019-02-22 13:22:25 +0100

    Added additional information to Rastrigin2D

--- SampleObjFuncs/Rastrigin2D.m
+++ SampleObjFuncs/Rastrigin2D.m
@@ -1,4 +1,6 @@
 function f_x = Rastrigin2D(x)
 	f_x = 20.0 + x(1)^2 + x(2)^2 - 10.0*(cos(2*pi*x(1)) + cos(2*pi*x(2)));
 end
+% x(i) in [-5.12, 5.12]. Global minimum at x=(0, 0) -> 0
 % See: https://es.mathworks.com/help/gads/example-rastrigins-function.html
+% See: https://en.wikipedia.org/wiki/Rastrigin_function
